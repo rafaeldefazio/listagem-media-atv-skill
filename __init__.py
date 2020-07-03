d8cd5745498944a95ac4994e4b6a588266c1a060
Rafael Biagioni de Fazio <rafaelbdefazio@gmail.com>
2020-07-03 19:46:51 -0300

    fix

--- __init__.py
+++ __init__.py
@@ -1,15 +1,23 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import intent_file_handler, MycroftSkill, intent_handler
+from mycroft.skills.context import adds_context, removes_context
+import requests
 
 
 class ListagemMediaAtv(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+	def __init__(self):
+		MycroftSkill.__init__(self)
 
-    @intent_file_handler('atv.media.listagem.intent')
-    def handle_atv_media_listagem(self, message):
-        self.speak_dialog('atv.media.listagem')
+	@intent_file_handler('atv.media.listagem.intent')
+	def handle_atv_media_listagem(self, message):
+
+		r = requests.get('http://localhost:3000/media-atividades-30dias')
+		rn = r.json()[0]
+
+		for ri in rn:
+
+			self.speak_dialog('atv.media.listagem', data=ri)
 
 
 def create_skill():
-    return ListagemMediaAtv()
+	return ListagemMediaAtv()
 
